f74f5d5f3edcb2bea558869d4ba1dd097e9a50b1
ewrfun <asemanrafaterfan@gmail.com>
2024-07-05 00:16:40 +0330

    massage

---
+++ resources/views/welcome.blade.php
@@ -0,0 +1,163 @@
+<!DOCTYPE html>
+<html lang="en">
+
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <title>soheil khaledabadi - Developer</title>
+
+    <link rel="preconnect" href="https://fonts.googleapis.com">
+    <link rel="preconnect" href="https://fonts.gstatic.com" crossorigin>
+    <link href="https://fonts.googleapis.com/css2?family=Open+Sans:wght@300&family=Raleway:wght@100&family=Roboto+Mono:wght@100&display=swap" rel="stylesheet">
+    <style>
+        body {
+            margin: 0;
+            padding: 0;
+            color: #333;
+        }
+
+        h1 {
+            font-family: 'Open Sans', sans-serif;
+            font-family: 'Raleway', sans-serif;
+            font-family: 'Roboto Mono', monospace;
+        }
+
+        header {
+            padding: 1em 0;
+            box-shadow: 0px 2px 4px rgba(0, 0, 0, 0.1);
+        }
+
+        nav {
+            margin-left: 20px;
+        }
+
+        nav ul {
+            list-style: none;
+            padding: 0;
+        }
+
+        nav ul li {
+            display: inline;
+            margin-right: 20px;
+        }
+
+        nav ul li a {
+            text-decoration: none;
+            color: #333;
+        }
+
+        .content-section {
+            max-width: 800px;
+            margin: 0 auto;
+            padding: 20px;
+            color: #333;
+        }
+
+        h1 {
+            font-size: 36px;
+            margin-top: 0;
+            color: #333;
+        }
+
+        h2 {
+            font-size: 24px;
+            color: #333;
+        }
+
+        p {
+            font-size: 18px;
+            line-height: 1.6;
+            color: #555;
+        }
+
+        ul {
+            list-style-type: disc;
+            padding-left: 20px;
+        }
+
+        .articles-page {
+            max-width: 800px;
+            margin: 0 auto;
+            padding: 20px;
+            color: #333;
+        }
+
+        .article {
+            margin-bottom: 20px;
+        }
+
+        #links {
+            padding: 20px;
+            margin-top: 20px;
+        }
+
+        #links h1 {
+            font-size: 24px;
+            color: #333;
+        }
+
+        #links ul {
+            list-style: none;
+            padding: 0;
+        }
+
+        #links li {
+            margin-bottom: 10px;
+        }
+
+        #links a {
+            text-decoration: none;
+            color: #0073e6;
+            /* Link color */
+            font-weight: bold;
+        }
+
+        #links a:hover {
+            color: #0055a5;
+        }
+
+        /* Hover color */
+
+        footer {
+            padding: 10px 0;
+            text-align: center;
+            color: #555;
+        }
+
+
+        @media screen and (max-width: 600px) {
+            h1 {
+                font-size: 28px;
+            }
+
+            h2 {
+                font-size: 20px;
+            }
+
+            p {
+                font-size: 16px;
+            }
+        }
+    </style>
+
+</head>
+
+<body>
+    <header>
+        <nav>
+            <ul>
+                <li><a href="#about">About Me</a></li>
+                <li><a href="#skills">Skills</a></li>
+                <li><a href="#experience">Experience</a></li>
+                <li><a href="#projects">Projects</a></li>
+                <li><a href="#links">Links</a></li>
+            </ul>
+        </nav>
+    </header>
+
+
+
+
+
+
+</html>
